79b9f610acb1096a5dcbb8650b42becb40416bbc
MuthuVijay96 <muthuvijay96@gmail.com>
2023-12-07 19:32:55 +0530

    VijayProgram

---
+++ src/test/java/vijay/VijayProgram.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+package vijay;
+
+public class VijayProgram {
+	
+public void mani() {
+	System.out.println("vijay");
+}
+	public static void main(String[]args) {
+		VijayProgram n = new VijayProgram();
+	         n.mani();
+	}
+	
+}
