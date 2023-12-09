4967b26d1ed837272c4d2c3ed84720d81bc146fb
sugunamithran <suguna.m1994@gmail.com>
2023-12-09 23:03:32 +0530

    Updated New Class Test_1

---
+++ src/test/java/com/baseclass/Test_1.java
@@ -0,0 +1,14 @@
+package com.baseclass;
+
+public class Test_1 {
+	public static void main(String[] args)
+	{
+		String s = "Welcome to Project class";
+		for(int i=0;i<s.length();i++)
+		{
+			char c = s.charAt(i);
+			System.out.println(c);
+		}
+	}
+
+}
