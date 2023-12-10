0809c2a3ad1c0f69fbb3a34e4e5dc4f5b8133e16
samueljoys <samueljoys1898@gmail.com>
2023-12-10 10:01:53 +0530

    Updated New Class Sample

--- src/test/java/com/baseclass/Sample.java
+++ src/test/java/com/baseclass/Sample.java
@@ -1,5 +1,17 @@
 package com.baseclass;
 
 public class Sample {
-
+	public static void main(String[]args) {
+		int a=0, b=1,c=0;
+		System.out.println(a);
+		System.out.println(b);
+		for(int i=0;i<5;i++) {
+			c=a+b;
+			System.out.println(c);
+			a=b;
+			b=c;
+			
+		}
+		}
 }
+
