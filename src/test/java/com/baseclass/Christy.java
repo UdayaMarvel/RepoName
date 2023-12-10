2159591e951d9ae73f3fb60ba9e680f9255a49e4
christy-ary <christythomas3141170@gmail.com>
2023-12-10 17:18:31 +0300

    christy

--- src/test/java/com/baseclass/Christy.java
+++ src/test/java/com/baseclass/Christy.java
@@ -2,23 +2,12 @@ package com.baseclass;
 
 
 public class Christy {
-	public void bottleInfo(String Quality) {
-        System.out.println("Bottle Quality: "+Quality);
-    }
-         public void bottleInfo(String Quality,int price) {
-         System.out.println("Bottle Quality: "+Quality);
-         System.out.println("Bottle Price: "+price);
-    }
-        public void bottleInfo(String Quality,int price,String color) {
-        System.out.println("Bottle Quality: "+Quality);
-        System.out.println("Bottle Price: "+price);
-        System.out.println("Bottle color: "+color);
-    }
- 
+	
         public static void main(String[] args) {
-        	Christy B=new Christy();
-            B.bottleInfo("good",25);
-            B.bottleInfo("good",25,"Blue");
-           
+        	String s="Welcome to Java";
+        	String[] cs=s.split(" ");
+        	for(int i=0;i<=cs.length;i++) {
+        		System.out.println(cs[i]);
+        	}
         }
 }
