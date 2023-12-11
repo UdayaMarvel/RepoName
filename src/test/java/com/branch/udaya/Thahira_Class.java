09d89ff47c9b82d60407b748ad6db542e28a2c68
ThahiraBegam <thahirathahi1823@gmail.com>
2023-12-11 17:03:06 +0530

    [200~int factorial=1;

--- src/test/java/com/branch/udaya/Thahira_Class.java
+++ src/test/java/com/branch/udaya/Thahira_Class.java
@@ -3,8 +3,11 @@ package com.branch.udaya;
 public class Thahira_Class {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int factorial=1;
+		for(int i=7; i>=1; i--) {
+			factorial=factorial*i;
+		}
+		System.out.println("Factorial of 7:"+factorial);
 	}
 
 }
